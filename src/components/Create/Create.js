7672ec225460f76ed0455fec5cb99b8b0a5b3f83
haesooKr <gotn676@gmail.com>
2020-03-23 20:34:10 -0400

    Create Create folder

---
+++ src/components/Create/Create.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Create = () => {
+  return (
+    <div>
+      Create
+
+      {/* 
+          방만들기
+             라운드
+             타이머 시간제한
+             Language
+             시작버튼
+             플레이어 리스트
+             링크 
+            */}
+    </div>
+  )
+}
+
+export default Create;
